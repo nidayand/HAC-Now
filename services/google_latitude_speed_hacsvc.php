4fa169fedf836d874ef41c1da00a1216a2c2d150
Peter Gothager <pgweb@gothager.se>
2013-01-15 13:47:59 +0100

    Added verification of speed and accuracy

--- services/google_latitude_speed_hacsvc.php
+++ services/google_latitude_speed_hacsvc.php
@@ -42,9 +42,11 @@ namespace google_latitude_speed_hacsvc {
 				array("key"=>"client_id", "value"=>null, "mandatory"=>1,"description"=>"Go to https://code.google.com/apis/console/ and generate a 'Client ID for installed applications'. Sign up for the Calender API service"),
 				array("key"=>"client_secret", "value"=>null, "mandatory"=>1,"description"=>"Same as for client_id. The client_secret is needed for creating a new token if the previous one has expired"),
 				array("key"=>"scope", "value"=>"https://www.googleapis.com/auth/latitude.current.best", "mandatory"=>1,"description"=>"The scope of the request. Not necessary to change this value"),
-				array("key"=>"device_code", "value"=>null, "mandatory"=>2,"description"=>"Device code generated"),
+				array("key"=>"device_code", "value"=>null, "mandatory"=>2,"description"=>"Device code generated. Internal usage only"),
 				array("key"=>"user_code", "value"=>null, "mandatory"=>2,"description"=>"Code to be used with the URL for registration to an account"),
-				array("key"=>"verification_url", "value"=>null, "mandatory"=>2,"description"=>"URL for authentication")
+				array("key"=>"verification_url", "value"=>null, "mandatory"=>2,"description"=>"URL for authentication"),
+				array("key"=>"speed_limit", "value"=>"5", "mandatory"=>1,"description"=>"If speed limit is above this value (in m/s) the map will be displayed. Use Google to translate from e.g. km/h to m/s via a simpel search - 20 km/h to m/s"),
+				array("key"=>"accuracy_limit", "value"=>"30", "mandatory"=>1,"description"=>"The map will only be displayed if the accuracy of the position is less that the value")
 		);
 	}
 
@@ -67,13 +69,13 @@ namespace google_latitude_speed_hacsvc {
 			kvp_set("device_code",$setup_data_new["device_code"]);
 			kvp_set("user_code",$setup_data_new["user_code"]);
 			kvp_set("verification_url",$setup_data_new["verification_url"]);
-		
+
 			$setup_data = $setup_data_new;
 		} else {
 			debug("Failed to retrieve device_code..");
 			return false;
 		}
-		
+
 		/*
 		 * Check for access token
 		*/
@@ -81,7 +83,7 @@ namespace google_latitude_speed_hacsvc {
 		if ($setup_data_new && array_diff($setup_data, $setup_data_new)){
 			//Store the information
 			kvp_set("access_token",$setup_data_new["access_token"]);
-		
+
 			$setup_data = $setup_data_new;
 		} else {
 			debug("Failed to retrieve access_token..");
@@ -91,7 +93,7 @@ namespace google_latitude_speed_hacsvc {
 		$client = new \Google_Client();
 		$client->setApplicationName('HAC Latitude Speed');
 		$client->setClientId($setup_data["client_id"]);
-		$client->setClientSecret($setup_data["client_secret"]);		
+		$client->setClientSecret($setup_data["client_secret"]);
 		$client->setScopes(array($setup_data["scope"]));
 		$client->setAccessToken($setup_data["access_token"]);
 
@@ -100,21 +102,29 @@ namespace google_latitude_speed_hacsvc {
 
 		if ($client->getAccessToken()) {
 			$currentLocation = $latitude->currentLocation->get(array("granularity"=>"best"));
-			
+				
 			debug("Current location: ".json_encode($currentLocation));
 
 			// We're not done yet. Remember to update the cached access token.
 			// Remember to replace $_SESSION with a real database or memcached.
 			kvp_set("access_token", $client->getAccessToken());
-			
-			return $currentLocation;
+
+			/*
+			 * Only return if there is a speed value and it is equal or exceeds the predefined value and the 
+			 * accuracy is equal or less than the predefined value
+			 */
+			if (isset($currentLocation["speed"]) && $currentLocation["speed"]>= $setup_data["speed_limit"] 
+					&& $currentLocation["accuracy"]<=$setup_data["accuracy_limit"]){
+				debug("Location and speed is reported");
+				return $currentLocation;
+			}
 		}
-		return false;
+		return null;
 	}
 	const javascript = <<<EOT
 	this.content = function (widget, ui, data){
 		var html = '<table width="100%" border="0"><tbody><tr><td><img src="https://maps.googleapis.com/maps/api/staticmap?center='+data.latitude+','+data.longitude+'&zoom=12&size=400x200&markers=color:blue%7Clabel:S%7C'+data.latitude+','+data.longitude+'&sensor=false"/></td></tr></tbody></table>';
-			
+		
 		//Update the widget
 		widget.infobox("option",{
 			"priority" : 1,
@@ -122,7 +132,7 @@ namespace google_latitude_speed_hacsvc {
 			"headline" : ui.title,
 			"subheadline" : null,
 			"contentpadding" : true
-		});			
+		});
 	}
 EOT;
 
